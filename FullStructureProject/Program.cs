147e939860f9b5e636d173e374c34d9c397f0fb9
Lu.Bu <phamhoangminhchau1973@gmail.com>
2025-05-16 11:22:33 +0700

    Add project files.

---
+++ FullStructureProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace FullStructureProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(":D \"...\" okay ");
+
+            Console.WriteLine("Cac doi so dau vao cua ban");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleCalculation/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+void do_calculation(double a, double b, string op)
+{
+    switch (op)
+    {
+        case "+":
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            break;
+        case "-":
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+            break;
+        case "*":
+            Console.WriteLine("{0} * {1} = {2}", a, b, a*b);
+            Console.WriteLine($" {a} +  { b} = { a+b}");
+            break;
+        case "/":
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            break;
+        default:
+            Console.WriteLine("Nhap lui ha ban");
+            break;
+    }
+}
+
+Console.WriteLine("Simple Calculation");
+Console.WriteLine("Nhap so a: ");
+double a = Double.Parse(Console.ReadLine());
+Console.WriteLine("Nhap so b:");
+double b = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Nhap phep toan + - * /");
+string  op = Console.ReadLine();
+do_calculation(a, b, op);
+Console.ReadLine();
