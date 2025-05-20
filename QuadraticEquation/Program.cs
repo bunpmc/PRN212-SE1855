db7a51dcfc2ff80eac55c7e125931ea5828afdb6
Lu.Bu <phamhoangminhchau1973@gmail.com>
2025-05-20 09:55:09 +0700

    20/5/2025

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using System.Text;
+
+void first_degree_solution (double a, double b)
+{
+    if ( a == 0 && b ==0 )
+    {
+        Console.WriteLine("Vo so nghiem");
+    }
+    else if ( a == 0 && b != 0) 
+    {
+        Console.WriteLine("Vo nghiem");
+    }else
+    {
+        Console.WriteLine($"x = {-b / a}");
+    }
+}
+
+void quadratic_equation_solution(double a, double b, double c)
+{
+    if (a == 0)
+    {
+        first_degree_solution(b, c);
+    } else
+    {
+        var delta = Math.Pow(b, 2) - 4 * a * c;
+        if(delta < 0)
+        {
+            Console.WriteLine("Phuong trinh vo nghiem");
+
+        } else if (delta > 0 )
+        {
+            var x1 = (-b-Math.Sqrt(delta))/(2*a);
+            var x2 = (-b+Math.Sqrt(delta))/(2*a);
+            Console.WriteLine("X1 = {0}, X2 = {1}",x1, x2);
+        } else
+        {
+            Console.WriteLine($"{-b / (2 * a)}");
+        }
+    }
+}
+
+Console.OutputEncoding = Encoding.UTF8;
+while (true)
+{
+    Console.WriteLine("Phuong trinh bac 2: ax^2 + bx + c = 0");
+    Console.WriteLine("He so a: ");
+    double a = Double.Parse(Console.ReadLine());
+    Console.WriteLine("He so b: ");
+    double b = Double.Parse(Console.ReadLine());
+    Console.WriteLine("He so c: ");
+    double c = Double.Parse(Console.ReadLine());
+
+    quadratic_equation_solution(a, b, c);
+    Console.ReadLine();
+}
---
+++ StringInteraction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string ho = "Nguyen";
+string tenlot = "Thi";
+String ten = "Teo";
+
+//Not recommended moi dau + la xin 1 o nho
+string fullname = ho + " " + tenlot  + " " + ten;   
+Console.WriteLine(fullname);
+
+//Chi cap 1 o nho 1 lan
+StringBuilder sb = new StringBuilder();
+sb.Append(ho);
+sb.Append(" ");
+sb.Append(tenlot);
+sb.Append(" ");
+sb.Append(ten);
+Console.WriteLine(sb.ToString());
