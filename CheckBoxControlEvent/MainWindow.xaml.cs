871f3b46049b697bb5c0e1702c7f827d43644a35
Lu.Bu <phamhoangminhchau1973@gmail.com>
2025-06-17 10:22:14 +0700

    CheckBoxControlEvent

---
+++ CheckBoxControlEvent/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBoxControlEvent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            chkCongNghe.IsChecked = true;
+            chkDuLich.IsChecked = true;
+            chkDaBong.IsChecked = true;
+            chkXemPhim.IsChecked = true;
+        }
+
+        private void chkToanBo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkXemPhim.IsChecked = false;
+            chkDaBong.IsChecked = false;
+            chkDuLich.IsChecked = false;
+            chkCongNghe.IsChecked = false;
+        }
+        private void chkSub_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            if(chkCongNghe.IsChecked == true && 
+                chkDuLich.IsChecked == true && 
+                chkDaBong.IsChecked == true &&
+                chkXemPhim.IsChecked == true)
+            {
+                chkToanBo.IsChecked = true;
+            } else if (chkCongNghe.IsChecked == false &&
+                chkDuLich.IsChecked == false &&
+                chkDaBong.IsChecked == false &&
+                chkXemPhim.IsChecked == false)
+            {
+                chkToanBo.IsChecked = false;
+            }
+        }
+    }
+}
